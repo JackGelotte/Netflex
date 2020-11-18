2cc46f6ed047ea9b6c21fec692c4b364e4b940c2
Jack Gelotte <jackgelotte@gmail.com>
2020-11-18 10:07:30 +0100

    Add project files.

---
+++ DatabaseConnection/Context.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseConnection
+{
+    public class Context : DbContext
+    {
+        public DbSet<UserAccount> UserAccounts { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<UserPassword> UserPasswords { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(
+                @"server=.\SQLExpress;" +
+                @"database=NetflexDatabase;" +
+                @"trusted_connection=true;" +
+                @"MultipleActiveResultSets=True"
+                );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ DatabaseConnection/Model.cs
@@ -0,0 +1,40 @@
+﻿ using System;
+using System.Collections.Generic;
+
+namespace DatabaseConnection
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNumber { get; set; }
+        // References to other tables
+        public virtual UserPassword UserPassword { get; set; }
+        public virtual ICollection<Rental> Rentals { get; set; }
+    }
+    public class UserPassword
+    {
+        public int Id { get; set; }
+        public int UserAccountId { get; set; }
+        public  virtual UserAccount UserAccount { get; set; }
+    }
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        // References to other tables
+        public virtual ICollection<Rental> Rentals { get; set; }
+    }
+    public class Rental
+    {
+        public int Id { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        // References to other tables
+        public virtual UserAccount UserAccount { get; set; }
+        public virtual Movie Movie { get; set; }
+    }
+}
